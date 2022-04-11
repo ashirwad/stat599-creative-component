e7764af3211aef50182228c7c67ddf2385cdcf2f
ashirwad <ashirwad1992@gmail.com>
2022-04-11 13:05:27 -0500

    Create EDA report

--- code/eda.R
+++ code/eda.R
@@ -28,3 +28,13 @@ options(datatable.na.options = c("", "NA")) # read these strings as NA
 # Import IST data
 ist_raw <- import(here("data", "IST_corrected.csv"))
 
+# Create EDA report
+ist_raw %>%
+  select(RDELAY, RSBP, RDEF1:RDEF8) %>%
+  eda_web_report(
+    output_file = "detailed-eda.html",
+    output_dir = here("docs"),
+    subtitle = "International Stroke Trial",
+    author = "Ashirwad Barnwal",
+    sample_percent = 10
+  )
