bb54f2751a0f891858ddbe086be00664b5cf30f4
ashirwad <ashirwad1992@gmail.com>
2022-04-11 11:41:38 -0500

    Define setup chunk

---
+++ code/eda.R
@@ -0,0 +1,25 @@
+## -----------------------------------------------------------------------------
+##
+## Purpose of script:
+##
+## Author: Ashirwad Barnwal
+##
+## Date Created: 2022-04-11
+##
+## Copyright (c) Ashirwad Barnwal, 2022
+## Email: ashirwad@iastate.edu; ashirwad1992@gmail.com
+##
+## -----------------------------------------------------------------------------
+##
+## Notes:
+##
+##
+## -----------------------------------------------------------------------------
+
+# Setup chunk
+library(conflicted)
+library(tidyverse)
+library(rio)
+library(dlookr)
+
+options(datatable.na.options = c("", "NA")) # read these strings as NA
