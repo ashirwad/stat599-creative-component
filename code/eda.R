9eba4ec59f83b3bf0e43c0deb4e9acf9415628d7
ashirwad <ashirwad1992@gmail.com>
2022-04-11 14:33:34 -0500

    Import IST as rds file

--- code/eda.R
+++ code/eda.R
@@ -29,40 +29,7 @@ library(dlookr)
 options(datatable.na.strings = c("", "NA")) # read these strings as NA
 
 # Import IST data
-ist_raw <- import(here("data", "IST_corrected.csv"))
-
-# Clean IST data
-ist <- ist_raw %>%
-  select(RDELAY:STYPE, RXASP, RXHEP, OCCODE) %>%
-  mutate(
-    RXHEP = fct_collapse(RXHEP, "M" = c("M", "H")),
-    treatment = case_when(
-      RXASP == "N" & RXHEP == "N" ~ "no_asp_no_hep",
-      RXASP == "N" & RXHEP == "L" ~ "no_asp_low_hep",
-      RXASP == "N" & RXHEP == "M" ~ "no_asp_med_hep",
-      RXASP == "Y" & RXHEP == "N" ~ "yes_asp_no_hep",
-      RXASP == "Y" & RXHEP == "L" ~ "yes_asp_low_hep",
-      RXASP == "Y" & RXHEP == "M" ~ "yes_asp_med_hep"
-    ),
-    treatment = fct_relevel(treatment, "no_asp_no_hep") %>%
-      fct_relevel("yes_asp_no_hep", after = 3),
-    OCCODE = recode(
-      OCCODE, `1` = "dead", `2` = "dependent", `3` = "not_recovered",
-      `4` = "recovered", .default = "missing"
-    ),
-    dead_or_dep = case_when(
-      OCCODE %in% c("dead", "dependent") ~ "yes",
-      OCCODE %in% c("not_recovered", "recovered") ~ "no",
-      OCCODE == "missing" ~ "missing"
-    ) %>%
-      fct_relevel("no")
-  ) %>%
-  select(-RXASP, -RXHEP, -OCCODE) %>%
-  mutate(across(where(is.character), ~ as.factor(.x))) %>%
-  filter(
-    !is.na(RATRIAL), # discard pilot phase data due to incompleteness
-    dead_or_dep != "missing"
-  )
+ist <- here("output", "rds-files", "ist.rds")
 
 # Create EDA report
 ist %>%
@@ -70,6 +37,5 @@ ist %>%
     output_file = "detailed-eda.html",
     output_dir = here("docs"),
     subtitle = "International Stroke Trial",
-    author = "Ashirwad Barnwal",
-    sample_percent = 10
+    author = "Ashirwad Barnwal"
   )
